c27de6b5ac46f577c52c75dae37898f9046440c5
dmr121 <dmrozmajzl@gmail.com>
2023-09-27 14:20:54 -0400

    Initial Commit

---
+++ Color Picker/Color_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_PickerApp.swift
+//  Color Picker
+//
+//  Created by David Rozmajzl on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Color_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
